89b5b7cce12b81ef66bc9129bd77acff2a586a0e
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-10-28 11:55:55 -0700

    add question and answer toggle buttons

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export default Ember.Component.extend({
+  newAnswerHidden: true,
   actions: {
     postAnswer() {
       var params = {
@@ -13,10 +14,17 @@ export default Ember.Component.extend({
       if(params.author === '' || params.body === undefined) {
         alert("It looks like you still have an empty field in your answer form!");
       } else {
+        this.set('newAnswerHidden', true);
         this.sendAction('postAnswer', params);
         this.set('author', '');
         this.set('body', '');
       }
+    },
+    showNewAnswer() {
+      this.set('newAnswerHidden', false);
+    },
+    cancelAnswer() {
+      this.set('newAnswerHidden', true);
     }
   }
 });
--- app/components/new-question.js
+++ app/components/new-question.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export default Ember.Component.extend({
+  newQuestionHidden: true,
   actions: {
     postQuestion() {
       var params = {
@@ -13,11 +14,18 @@ export default Ember.Component.extend({
       if (params.author === '' || params.title === undefined || params.body === undefined) {
         alert("It looks like you still have an empty field in your question form!");
       } else {
+        this.set('newQuestionHidden', true)
         this.sendAction('postQuestion', params);
         this.set('author', '');
         this.set('title', '');
         this.set('body', '');
       }
+    },
+    showNewQuestion() {
+      this.set('newQuestionHidden', false);
+    },
+    cancelQuestion() {
+      this.set('newQuestionHidden', true);
     }
   }
 });
