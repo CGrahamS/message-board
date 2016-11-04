8c9cc65739d0398e22624c66b7f187ca8be124be
Caleb Stevenson <Guest@epicodus-44.local>
2016-11-04 08:56:53 -0700

    add total answer computed property

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  totalAnswers: Ember.computed('question.answers', function() {
+    if (this.get('question.answers.length') > 0) {
+      return Ember.String.htmlSafe('Answers: ' + this.get('question.answers.length'));
+    } else {
+      return Ember.String.htmlSafe('<h5>No answers yet!</h5>');
+    }
+  })
 });
